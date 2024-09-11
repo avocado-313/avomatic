4a36a38780113f380ff0b2744ef45632271cf8b6
karim_twerlo <karim.salem@twerlo.com>
2024-09-11 19:54:21 +0300

    framework handover

--- src/main/java/avocado/P01AvocadoLogin.java
+++ src/main/java/avocado/P01AvocadoLogin.java
@@ -103,6 +103,9 @@ public class P01AvocadoLogin extends PageBase {
         clickOnElement(login_CTA);
         waitForVisibilityOfElement(avocado_logo_from_home);
     }
+    public void test(){
+        System.out.println("nothing!");
+    }
 
 
 

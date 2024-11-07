116be0dba3ffdaf025f4982edb81b357a4a4e272
ArpitBaranwal18 <arpit.b@twerlo.com>
2024-11-07 17:19:46 +0530

    1. Fixes the locators issues 2. Fixes the issues in Search and Contacts modules

--- src/main/java/avocado/P02AvocadoHomeScreen.java
+++ src/main/java/avocado/P02AvocadoHomeScreen.java
@@ -9,17 +9,19 @@ public class P02AvocadoHomeScreen extends PageBase {
     public P02AvocadoHomeScreen(WebDriver driver) {
         super(driver);
     }
+    private final By analytics_icon_from_menu = By.xpath("//a[@aria-label='Analytics']");
     private final By search_icon_from_menu = By.xpath("//button[@aria-label='Search']");
     private final By chat_icon_from_menu = By.xpath("//a[@aria-label='Chat']");
     private final By media_icon_from_label = By.xpath("//a[@aria-label='Media Library']");
     private final By contact_icon_from_label = By.xpath("//a[@aria-label='Contacts']");
     private final By contains_icon_from_label = By.xpath("//*[@aria-label='E-commerce Apps']");
-    private final By settings_icon_from_menu = By.xpath("//a[@aria-label='Settings']//*[name()='svg']");
+    private final By broadcast_icon_from_menu = By.xpath("//a[@aria-label='Broadcast']");
+    private final By settings_icon_from_menu = By.xpath("//a[@aria-label='Settings']");
     private final By subscription_billing_icon_from_menu = By.xpath("//button[@value='Billing']//*[name()='svg']");
     private final By language_icon_from_menu = By.xpath("//*[@aria-label='English Language']");
 //    private final By notification_icon_from_menu = By.xpath("//a[@aria-label='Notifications']//*[name()='svg']");
     private final By account_icon_from_menu = By.xpath("//button[@aria-label='Profile']");
-    private final By apps_from_menu = By.xpath("//a[@aria-label='Apps']//*[name()='svg']");
+    private final By apps_from_menu = By.xpath("//a[@aria-label='Apps']");
     private final By new_chat = By.xpath("//button[normalize-space()='+ New Chat']");
     private final By search_title = By.xpath("(//h5[normalize-space()='Search'])[1]");
     private final By media_library_title = By.xpath("(//p[@class='MuiTypography-root MuiTypography-body1 css-1hxh1sq'])[1]");
@@ -27,7 +29,7 @@ public class P02AvocadoHomeScreen extends PageBase {
     private final By apps_title = By.xpath("(//p[@class='MuiTypography-root MuiTypography-body1 css-1hxh1sq'])[1]");
 
     public void checkHomeScreen(){
-        ElementsValidator(search_icon_from_menu,chat_icon_from_menu,media_icon_from_label,contact_icon_from_label
+        ElementsValidator(analytics_icon_from_menu,search_icon_from_menu,chat_icon_from_menu,media_icon_from_label,contact_icon_from_label,broadcast_icon_from_menu
         ,contains_icon_from_label,settings_icon_from_menu,subscription_billing_icon_from_menu,language_icon_from_menu,account_icon_from_menu);
     }
     private void clickAndValidate(By by1, By by2){

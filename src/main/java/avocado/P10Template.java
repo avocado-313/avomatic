fb15ab789dba51dd82db71aabbc3bb22da77b54c
ArpitBaranwal18 <arpit.b@twerlo.com>
2025-01-17 20:07:41 +0530

    Fixes the template module

--- src/main/java/avocado/P10Template.java
+++ src/main/java/avocado/P10Template.java
@@ -2,8 +2,15 @@ package avocado;
 
 import PageBase.PageBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.KeyInput;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class P10Template extends PageBase {
     public P10Template(WebDriver driver) {
@@ -29,13 +36,28 @@ public class P10Template extends PageBase {
     private final By image_media = By.xpath("(//*[normalize-space()='Image'])[1]");
     private final By video_media = By.xpath("(//*[normalize-space()='Video'])[1]");
     private final By docs_media = By.xpath("(//*[normalize-space()='Docs'])[1]");
-    private final By body_text = By.xpath("(//*[normalize-space()='Body Text'])[1]");
+    private final By body_text = By.xpath("//*[@class ='MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-fullWidth MuiInputBase-formControl MuiInputBase-multiline css-1sbjj2w']");
     private final By add_footer_text = By.xpath("//*[@placeholder='Add Footer Text']");
     private final By buttons = By.xpath("//*[normalize-space()='Buttons']");
-    private final By none_buttons = By.xpath("(//*[normalize-space()='None'])[3]");
+    private final By none_buttons = By.xpath("//button[@value='none']");
     private final By CTA_Buttons = By.xpath("//*[@data-testid='button-type-cta']");
     private final By quick_replies_button = By.xpath("//*[@data-testid='button-type-quick-reply']");
     private final By whats_app_message_preview = By.xpath("//*[@data-testid='preview-whatsapp-msg']");
+    private final By insertVariable = By.xpath("(//*[normalize-space() = 'INSERT VARIABLE {{#}}'])[2]");
+    private final By emoji = By.xpath("//*[@data-testid='select-emoji-picker']");
+    private final By smileyEmojis = By.xpath("(//*[normalize-space() = 'Smileys & People'])[2]");
+    private final By addCta = By.xpath("//button[.//span[contains(text(),'Next')] and .//span[contains(text(),'Saving...')] and .//span[text() = 'Saved'] and .//span[text() = 'Error']]");
+    private final By upload_image_CTA = By.xpath("//button[normalize-space() = 'Upload image']");
+//    private final By image = By.xpath("//input[@name='image']");
+
+    private final By enter_Variable = By.xpath("//*[normalize-space() = 'Enter the variable 1']");
+    private final By submit_ReviewCTA = By.xpath("//button[.//span[contains(text(),'Submit for review')] and .//span[contains(text(),'Saving...')] and .//span[text() = 'Saved'] and .//span[text() = 'Error']]");
+
+
+
+    Actions actions = new Actions(driver);
+    String currentDir = System.getProperty("user.dir");
+    String file_path = currentDir + "//images//avocad0.jpg";
 
     private void validateCreateTemplateElements(){
         ElementsValidator(template_title,template_sub_title,template_id_input_field,template_description_input_field,
@@ -64,4 +86,247 @@ public class P10Template extends PageBase {
         waitForVisibilityOfElement(template_id_input_field);
         validateCreateTemplateElements();
     }
+
+    public void validateCreateNewTemplate() throws InterruptedException {
+//        clickOnElement(submit_template_CTA);
+        sendTextToInputField("test" + generateRandomDigits(5), template_id_input_field);
+        sendTextToInputField("testing template" + generateRandomDigits(3), template_description_input_field);
+        clickOnElement(marketing_category);
+        clickOnElement(language_selection);
+        waitForVisibilityOfElement(select_language_input);
+        driver.findElement(select_language_input).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(select_language_input).sendKeys(Keys.DELETE);
+        sendTextToInputField("English", select_language_input);
+
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(none_media);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("This teemplate only for testing purpose").perform();
+//        clickOnElement(insertVariable);
+        clickOnElement(emoji);
+        waitForVisibilityOfElement(smileyEmojis);
+        clickOnElement(By.xpath("(//img[@alt='smiling face with 3 hearts'])[1]"));
+//        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+//        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("Powered_By_Mottasl!");
+
+        actions.pause(Duration.ofSeconds(4)).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(addCta);
+        Thread.sleep(5000);
+
+    }
+    private final By buttonTitle = By.xpath("//*[normalize-space() = 'Button 1']");
+    private final By buttonDisplayed = By.xpath("//*[normalize-space() = 'Button display text']");
+    private final By textInput = By.xpath("//input[@placeholder='Text']");
+    private final By addButton = By.xpath("//*[@data-testid='add-quick-reply-btn']");
+
+    private void validateQuickRepliesButtonElements(){
+        ElementsValidator(buttonTitle, buttonDisplayed, textInput, addButton);
+
+    }
+    public void validateMediaAndVariableTemplate() throws InterruptedException {
+//        clickOnElement(submit_template_CTA);
+        waitForVisibilityOfElement(template_id_input_field);
+        sendTextToInputField("test" + generateRandomDigits(4), template_id_input_field);
+        sendTextToInputField("testing template" + generateRandomDigits(3), template_description_input_field);
+        clickOnElement(marketing_category);
+        clickOnElement(language_selection);
+        waitForVisibilityOfElement(select_language_input);
+        driver.findElement(select_language_input).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(select_language_input).sendKeys(Keys.DELETE);
+        sendTextToInputField("English", select_language_input);
+
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(image_media);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("This teemplate only for testing purpose").perform();
+        clickOnElement(insertVariable);
+        clickOnElement(emoji);
+        waitForVisibilityOfElement(smileyEmojis);
+        clickOnElement(By.xpath("(//img[@alt='smiling face with 3 hearts'])[1]"));
+//        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+//        sendTextToInputField("Powered_By_Mottasl", add_footer_text);
+        clickOnElement(none_buttons);
+        clickOnElement(addCta);
+        waitForVisibilityOfElement(enter_Variable);
+        clickOnElement(upload_image_CTA);
+        actions.pause(Duration.ofSeconds(5)).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(By.xpath("//button[normalize-space() = 'Select']"));
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("abcd").perform();
+        clickOnElement(submit_ReviewCTA);
+        Thread.sleep(5000);
+
+    }
+
+    public void validateQuickReplyButtonTemplate() throws InterruptedException {
+//        clickOnElement(submit_template_CTA);
+        waitForVisibilityOfElement(template_id_input_field);
+        sendTextToInputField("test" + generateRandomDigits(4), template_id_input_field);
+        sendTextToInputField("testing template" + generateRandomDigits(3), template_description_input_field);
+        clickOnElement(utility_category);
+        clickOnElement(language_selection);
+        waitForVisibilityOfElement(select_language_input);
+        driver.findElement(select_language_input).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(select_language_input).sendKeys(Keys.DELETE);
+        sendTextToInputField("English", select_language_input);
+
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(image_media);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("This template only for testing purpose").perform();
+        clickOnElement(insertVariable);
+        clickOnElement(emoji);
+        waitForVisibilityOfElement(smileyEmojis);
+        clickOnElement(By.xpath("(//img[@alt='smiling face with 3 hearts'])[1]"));
+//        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+//        sendTextToInputField("Powered_By_Mottasl", add_footer_text);
+
+        clickOnElement(quick_replies_button);
+        waitForVisibilityOfElement(buttonDisplayed);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("Test").perform();
+
+
+        clickOnElement(addCta);
+        waitForVisibilityOfElement(enter_Variable);
+        clickOnElement(upload_image_CTA);
+        actions.pause(Duration.ofSeconds(5)).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(By.xpath("//button[normalize-space() = 'Select']"));
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("abcd").perform();
+        clickOnElement(submit_ReviewCTA);
+        Thread.sleep(5000);
+
+    }
+
+    private final By phoneTitle = By.xpath("(//*[normalize-space() = 'Phone number'])[2]");
+    private final By phoneInput = By.xpath("//*[@placeholder='Enter phone number']");
+    private final By phoneText = By.xpath("//*[@placeholder='Text']");
+    private final By addBtnCTA = By.xpath("//button[normalize-space() = '+ Add Button']");
+    private final By staticUrl = By.xpath("//*[normalize-space() = 'Static Link']");
+    private final By enterURl = By.xpath("(//*[@placeholder='Enter URL Here'])[1]");
+    private  final By staticText = By.xpath("(//*[@placeholder='Text'])[2]");
+    private final By dynamicURl = By.xpath("//*[normalize-space() = 'Dynamic Link']");
+    private  final By dynamicText = By.xpath("(//*[@placeholder='Text'])[3]");
+    private final By uploadVideoCta = By.xpath("(//*[normalize-space() = 'Upload video'])[3]");
+
+
+
+
+
+
+    public void validateCTATemplate() throws InterruptedException {
+//        clickOnElement(submit_template_CTA);
+        waitForVisibilityOfElement(template_id_input_field);
+        sendTextToInputField("test CTA" + generateRandomDigits(4),template_id_input_field);
+        sendTextToInputField("Test desc." +generateRandomDigits(3),template_description_input_field);
+        clickOnElement(utility_category);
+        clickOnElement(language_selection);
+        waitForVisibilityOfElement(select_language_input);
+        driver.findElement(select_language_input).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(select_language_input).sendKeys(Keys.DELETE);
+        sendTextToInputField("English", select_language_input);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(video_media);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("This template is for testing the automation suites of CTA").perform();
+        clickOnElement(insertVariable);
+        clickOnElement(emoji);
+        waitForVisibilityOfElement(smileyEmojis);
+        clickOnElement(By.xpath("(//img[@alt='smiling face with 3 hearts'])[1]"));
+//        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(CTA_Buttons);
+        waitForVisibilityOfElement(phoneTitle);
+        sendTextToInputField(generateRandomNumber(),phoneInput);
+        sendTextToInputField("Call Me",phoneText);
+        clickOnElement(addBtnCTA);
+        waitForVisibilityOfElement(staticUrl);
+        sendTextToInputField("www.google.com",enterURl);
+        sendTextToInputField("google",staticText);
+        clickOnElement(addBtnCTA);
+        waitForVisibilityOfElement(dynamicURl);
+        sendTextToInputField("dynamic",dynamicText);
+        clickOnElement(addCta);
+        waitForVisibilityOfElement(enter_Variable);
+        clickOnElement(uploadVideoCta);
+
+        actions.pause(Duration.ofSeconds(5)).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(By.xpath("//button[normalize-space() = 'Select']"));
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("abcd").perform();
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("https://test.app.avoacd0.dev").perform();
+        clickOnElement(submit_ReviewCTA);
+
+
+        Thread.sleep(5000);
+
+    }
+
+    private final By actionCTA = By.xpath("(//*[@data-testid='actions-button'])[1]");
+    private final By viewCta = By.xpath("//*[normalize-space() = 'View']");
+    private final By addLanguage_CTA = By.xpath("//*[normalize-space()='Add Language']");
+    private final By addLanguageTitle = By.xpath("//*[normalize-space()='Add a New Language']");
+
+    public void validateAddLangTemplate() throws InterruptedException {
+        clickOnElement(actionCTA);
+        waitForVisibilityOfElement(viewCta);
+        clickOnElement(addLanguage_CTA);
+        waitForVisibilityOfElement(addLanguageTitle);
+        Assert.assertTrue(driver.findElement(addLanguageTitle).getText().contains("Add a New Language"));
+        clickOnElement(language_selection);
+        waitForVisibilityOfElement(select_language_input);
+        driver.findElement(select_language_input).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(select_language_input).sendKeys(Keys.DELETE);
+        sendTextToInputField("English", select_language_input);
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        clickOnElement(addCta);
+        waitForVisibilityOfElement(enter_Variable);
+        clickOnElement(uploadVideoCta);
+
+        actions.pause(Duration.ofSeconds(5)).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(By.xpath("//button[normalize-space() = 'Select']"));
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("abcd").perform();
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("https://test.app.avoacd0.dev").perform();
+        clickOnElement(submit_ReviewCTA);
+
+
+        Thread.sleep(5000);
+
+
+    }
+
+    private final By duplicateCTA = By.xpath("//*[normalize-space() = 'Duplicate template']");
+
+    public void duplicateTemplateCreation() throws InterruptedException {
+        clickOnElement(actionCTA);
+        waitForVisibilityOfElement(viewCta);
+        Thread.sleep(2000);
+        clickOnElement(duplicateCTA);
+        Thread.sleep(2000);
+        waitForVisibilityOfElement(template_title);
+        driver.findElement(template_id_input_field).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(template_id_input_field).sendKeys(Keys.DELETE);
+        sendTextToInputField("duplicate Testing" + generateRandomDigits(4), template_id_input_field);
+
+        scrollToElement(addCta);
+        clickOnElement(addCta);
+        waitForVisibilityOfElement(enter_Variable);
+        clickOnElement(uploadVideoCta);
+
+        actions.pause(Duration.ofSeconds(5)).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        clickOnElement(By.xpath("//button[normalize-space() = 'Select']"));
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("abcd").perform();
+        actions.pause(Duration.ofSeconds(3)).sendKeys(Keys.TAB).sendKeys("https://test.app.avoacd0.dev").perform();
+        clickOnElement(submit_ReviewCTA);
+
+        waitForVisibilityOfElement(template_sub_title);
+        Thread.sleep(5000);
+
+    }
+
+    public void validateDeleteTemplate() throws InterruptedException {
+        clickOnElement(actionCTA);
+        waitForVisibilityOfElement(viewCta);
+        clickOnElement(By.xpath("//*[normalize-space() = 'Delete template']"));
+        waitForVisibilityOfElement(By.xpath("//*[normalize-space() = 'Delete Template']"),50);
+        clickOnElement(By.xpath("//*[normalize-space() = 'Confirm delete']"));
+        Thread.sleep(5000);
+    }
+
+
+
+
+
 }

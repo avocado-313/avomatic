be4640663eeaf01e0b4c49298e82bc0a9d0a5708
Syed-Ehsan <67308774+Syed-Ehsan@users.noreply.github.com>
2025-01-20 15:17:06 +0530

    Broadcat fix

--- src/test/java/avocadoTests/M08BroadcastTest.java
+++ src/test/java/avocadoTests/M08BroadcastTest.java
@@ -38,6 +38,7 @@ public class M08BroadcastTest extends BaseTest {
         broadcast.checkCreateBroadcastScreen();
         broadcast.selectTemplate();
         broadcast.completeRecipientScreen(CONTACT);
+        broadcast.checkNavigateBackIntoBroadcastScreen();
 
     }
     @Test

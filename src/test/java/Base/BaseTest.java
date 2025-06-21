60b7333d948a8021d8dd29247ae77a07354e068b
Ahmed zaky <151965756+ZakyTwrelo@users.noreply.github.com>
2025-06-21 16:22:19 +0300

    Update BaseTest.java

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,4 +1,5 @@
 package Base;
+
 import Utilities.ExtentReportManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,7 +17,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static Utilities.ExtentReportManager.*;
-
+import static Base.ReadProperties.*;
 
 public class BaseTest {
     public WebDriver driver;
@@ -37,41 +38,39 @@ public class BaseTest {
         switch (browser.toLowerCase()) {
             case "chrome":
                 ChromeOptions options = new ChromeOptions();
-                if(Objects.equals(remote, "true")) {
-                    
+                chromeDeviceScale(options);
+
+                if (Objects.equals(remote, "true")) {
                     options.addArguments("--headless=new");
-                    chromeDeviceScale(options);
+                }
 
-                    Map<String, Object> deviceMetrics = new HashMap<>();
-                    deviceMetrics.put("width", 1920);
-                    deviceMetrics.put("height", 1080);
-                    deviceMetrics.put("pixelRatio", 0.6);  // Scale factor
-                    Map<String, Object> mobileEmulation = new HashMap<>();
-                    mobileEmulation.put("deviceMetrics", deviceMetrics);
-                    mobileEmulation.put("userAgent", "Mozilla/5.0 ...");
-                    options.setExperimentalOption("mobileEmulation", mobileEmulation);
+                driver = new ChromeDriver(options);
 
-                    driver = new ChromeDriver(options);
-                    Map<String, Object> coordinates = new HashMap<>();
-                    coordinates.put("latitude", 31.2156);
-                    coordinates.put("longitude", 29.9553);
-                    coordinates.put("accuracy", 100);
-                    ((ChromeDriver) driver).executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
-                    driver = new ChromeDriver(options);
-                }else {
-                    chromeDeviceScale(options);
-                    driver = new ChromeDriver(options);
+                if (Objects.equals(remote, "true")) {
+                    try {
+                        Map<String, Object> coordinates = new HashMap<>();
+                        coordinates.put("latitude", 31.2156);
+                        coordinates.put("longitude", 29.9553);
+                        coordinates.put("accuracy", 100);
+                        ((ChromeDriver) driver).executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
+                    } catch (Exception e) {
+                        System.err.println("Failed to set geolocation override: " + e.getMessage());
+                    }
                 }
                 break;
+
             case "edge":
                 driver = new EdgeDriver();
                 break;
+
             case "firefox":
                 driver = new FirefoxDriver();
                 break;
+
             case "safari":
                 driver = new SafariDriver();
                 break;
+
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
@@ -80,43 +79,43 @@ public class BaseTest {
     }
 
     private void configureDriver() {
+        if (!Objects.equals(remote, "true")) {
+            driver.manage().window().maximize();
+        }
 
-
-
-        if(!Objects.equals(remote, "true"))driver.manage().window().maximize();
-
-        driver.get(ReadProperties.URL);
+        driver.get(URL); // from ReadProperties
     }
-    private void chromeDeviceScale(ChromeOptions options){
+
+    private void chromeDeviceScale(ChromeOptions options) {
         Map<String, Object> deviceMetrics = new HashMap<>();
         deviceMetrics.put("width", 1920);
         deviceMetrics.put("height", 1080);
-        deviceMetrics.put("pixelRatio", 1.0);  // Scale factor
+        deviceMetrics.put("pixelRatio", 1.0);
+
         Map<String, Object> mobileEmulation = new HashMap<>();
         mobileEmulation.put("deviceMetrics", deviceMetrics);
         mobileEmulation.put("userAgent", "Mozilla/5.0 ...");
+
         options.setExperimentalOption("mobileEmulation", mobileEmulation);
     }
-
-
 
     @BeforeMethod(alwaysRun = true)
     public void beforeMethod(Method method) throws IOException {
         ExtentReportManager.beforeMethod(method);
     }
-    @AfterMethod(alwaysRun = true )
-    public void A_takeScreenShot(ITestResult result) throws IOException, InterruptedException {
-        takeScreenshot(result,driver);
-        System.out.println("screenshot captured");
+
+    @AfterMethod(alwaysRun = true)
+    public void takeScreenshotAfterTest(ITestResult result) throws IOException, InterruptedException {
+        takeScreenshot(result, driver);
+        System.out.println("Screenshot captured");
     }
 
     @AfterClass(alwaysRun = true)
-    public void tearDown(){
-        if (driver != null){
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
         flushReport();
-        System.out.println("Report path is here:  " + reportName );
-
+        System.out.println("Report path is here: " + reportName);
     }
 }

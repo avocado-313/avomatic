5372ab436ea9f29b1e3631617b0336ed6d75531a
Ahmed zaky <151965756+ZakyTwrelo@users.noreply.github.com>
2025-06-21 16:44:05 +0300

    Update Regression.java

--- src/test/java/regression/Regression.java
+++ src/test/java/regression/Regression.java
@@ -13,27 +13,7 @@ public class Regression extends BaseTest {
     P02AvocadoHomeScreen home;
     P03AvocadoWorkSpace workSpace;
     P04AvocadoContacts contacts;
-    P05AvocadoApps apps;
-    P06AvocadoSearch search ;
-    P07AvocadoMedia media;
-    P08AvocadoTags tags;
-    P09BroadCast broadcast;
-    P10Template template;
-    P11QuickReplies reply;
-    P12RulesPage rule;
-    P013CustomAttributes custom;
-    P14ECommerce commerce;
-    P15ZidPage zid;
-    P16SallaPage salla;
-    P17ShopifyPage shopify;
-    P18WooCommercePage woo;
-    P19FooderEcommerceCard fooder;
-    P20Billing bill;
-    P30WhatsappWidget whatsapp;
-    P21Analytics analytics;
-    P22AppsCallReport appsCallReport;
-    P23AppsRoboCalling appsRoboCalling;
-    P24WebPhone webPhone;
+
 
     @BeforeClass
     public void initiateObjects(){
@@ -42,27 +22,7 @@ public class Regression extends BaseTest {
         home = new P02AvocadoHomeScreen(driver);
         workSpace = new P03AvocadoWorkSpace(driver);
         contacts = new P04AvocadoContacts(driver);
-        apps = new P05AvocadoApps(driver);
-        search = new P06AvocadoSearch(driver);
-        media = new P07AvocadoMedia(driver);
-        tags = new P08AvocadoTags(driver);
-        broadcast = new P09BroadCast(driver);
-        template = new P10Template(driver);
-        reply = new P11QuickReplies(driver);
-        rule = new P12RulesPage(driver);
-        custom = new P013CustomAttributes(driver);
-        zid = new P15ZidPage(driver);
-        salla = new P16SallaPage(driver);
-        commerce = new P14ECommerce(driver);
-        shopify = new P17ShopifyPage(driver);
-        woo = new P18WooCommercePage(driver);
-        fooder = new P19FooderEcommerceCard(driver);
-        bill = new P20Billing(driver);
-        whatsapp = new P30WhatsappWidget(driver);
-        analytics = new P21Analytics(driver);
-        appsCallReport = new P22AppsCallReport(driver);
-        appsRoboCalling = new P23AppsRoboCalling(driver);
-        webPhone = new P24WebPhone(driver);
+       
 
 
     }
@@ -101,7 +61,6 @@ public class Regression extends BaseTest {
         workSpace.checkWorkSpaceProfile();
         login.logout();
     }
- 
     @Test
     public void TC_005ValidateContacts() {
         login.loginToAvocado(username, password,Workspace);
@@ -110,5 +69,6 @@ public class Regression extends BaseTest {
         contacts.validateCreateContactCard();
         login.logout();
     }
+
 
 }
